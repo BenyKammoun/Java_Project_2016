17c4bfc767dcdc2e05218c161661f16137252755
Binyamine Kamoun <beny.kam@outlook.com>
2016-12-08 19:08:32 +0200

    business

--- app/src/main/java/com/a2016/proj/bensimhon/kamoun/java_project_2016/Model/Entities/business.java
+++ app/src/main/java/com/a2016/proj/bensimhon/kamoun/java_project_2016/Model/Entities/business.java
@@ -7,5 +7,30 @@ package com.a2016.proj.bensimhon.kamoun.java_project_2016.Model.Entities;
 public class Business {
 
     private int businessID;
-    private String name;
+    private String businessName;
+    private String businessAdress;
+
+    public int getBusinessID() {
+        return businessID;
+    }
+
+    public void setBusinessID(int businessID) {
+        this.businessID = businessID;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getBusinessAdress() {
+        return businessAdress;
+    }
+
+    public void setBusinessAdress(String businessAdress) {
+        this.businessAdress = businessAdress;
+    }
 }
